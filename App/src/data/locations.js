52891ddad42255eb45ec281e306ca5b24b1d7da1
Afonso Cunha <afonsocunha@Afonsos-MBP.Home>
2023-06-21 13:39:37 +0100

    Increased threshold to reduce false positives

--- App/src/data/locations.js
+++ App/src/data/locations.js
@@ -4,20 +4,20 @@ export const locations = [
     name: "Estação de comboios do Rossio",
     latitude: 38.714438,
     longitude: -9.140852,
-    recognitionThreshold: 0.98, 
+    recognitionThreshold: 0.99, 
   },
   {
     id: 2,
     name: "Palácio Pimenta",
     latitude: 38.758467,
     longitude: -9.156634,
-    recognitionThreshold: 0.98,
+    recognitionThreshold: 0.99,
   },
   {
     id: 3,
     name: "Interface de Transportes do Cais do Sodré",
     latitude: 38.705892,
     longitude: -9.144920,
-    recognitionThreshold: 0.98,
+    recognitionThreshold: 0.99,
   },
 ];
