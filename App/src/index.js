afdd4b88f26adc1b54028faf9b27e678510dc6d9
Afonso Cunha <afonsocunha@Afonsos-MBP.Home>
2023-06-22 10:37:41 +0100

    Remove unused import

--- App/src/index.js
+++ App/src/index.js
@@ -5,13 +5,12 @@ import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  //</React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
